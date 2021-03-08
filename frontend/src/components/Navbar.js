d141f4cce976a822d301c6a67e793efa3c6bce01
Karan Mann <karan.mann@outlook.com>
2021-03-08 14:03:53 +0100

    Addition Formatting added to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,27 +1,46 @@
 import React from 'react';
-import { NavbarContainer, LinkContainer, NavbarText } from '../lib/styled/styled'
+import { 
+  NavbarContainer, 
+  LinkContainer, 
+  NavbarText,
+  PageLinks,
+  SocialLinks,
+  YellowHighlight,
+  NavbarButton,
+  SocialImage
+  } from '../lib/styled/styled'
+import linkedin from  '../lib/images/linkedin.svg'
+import github from  '../lib/images/github.svg'
+
 
 export const Navbar = () => {
   return (
     <NavbarContainer>
       <NavbarText>
         <p><b>I'm Karan Mann.</b></p>
-        <p>A frontend developer with a background as a Chef</p>
+        <p>A 
+          <YellowHighlight>
+            frontend developer
+          </YellowHighlight>  
+          with a background as a Chef</p>
       </NavbarText>
       <LinkContainer>
-        <div>
-          <button href="#">Home</button>
-          <button href="#">CV</button>
-          <button href="#">Projects</button>
-          <button href="#">Blogs</button>
-          <button href="#">Login</button>
-        </div>
-        <div>
-          <button href="#">LinkedIn</button>
-          <button href="#">Github</button>
-        </div>
+        <PageLinks>
+          <NavbarButton href="#">Home</NavbarButton>
+          <NavbarButton href="#">CV</NavbarButton>
+          <NavbarButton href="#">Projects</NavbarButton>
+          <NavbarButton href="#">Blogs</NavbarButton>
+          <NavbarButton href="#">Login</NavbarButton>
+        </PageLinks>
+        <SocialLinks>
+          <NavbarButton href="#">
+            <SocialImage src={linkedin} alt="linkedin"/>
+          </NavbarButton>
+          <NavbarButton href="#">
+            <SocialImage src={github} alt="github"/>
+          </NavbarButton>
+        </SocialLinks>
       </LinkContainer>
-      
     </NavbarContainer>
   )
 }
--- frontend/src/lib/styled/styled.js
+++ frontend/src/lib/styled/styled.js
@@ -2,23 +2,42 @@ import styled from 'styled-components/macro'
 
 
 
-// BODY
+// UNIVERSAL
 
+export const YellowHighlight = styled.span`
+background-color: #f0ff00;
+padding: 0 4px
+`
 
 // NAVBAR
 
 export const NavbarContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center
+  align-items: center;
+  background-color: #f4f4f4
 `
 
 export const NavbarText = styled.div `
-  line-height: 10px
+  line-height: 18px
 `
 
 export const LinkContainer = styled.div`
   display: flex;
+  justify-content: space-between;
+  flex-direction: column
+`
 
-  justify-content: space-between
+export const PageLinks = styled.div `
+
+`
+export const SocialLinks = styled.div`
+
+`
+export const SocialImage = styled.img`
+  height:30px;
+  width: 30px
+`
+export const NavbarButton = styled.button`
+  border: none
 `
