1d24d2b6d0cca1a0016d8862a09c264c7762a797
Karan Mann <karan.mann@outlook.com>
2021-12-29 01:30:24 +0100

    New Updates

--- frontend/src/projects.js
+++ frontend/src/projects.js
@@ -1,281 +1,183 @@
-export const Data = 
-{
-  "projects": [
+export const Data = {
+  projects: [
     {
-      "title": "FOODEX",
-      "description": "A food site to focus on HTML, SASS-CSS, JS and Animations",
-      "image_url": "./projects/FOODEX.png",
-      "live_url": "https://project-foodex.netlify.app/",
-      "techs": [
+      title: "50 DAYS OF CODE",
+      description:
+        "50 Days 50 Projects. An ongoing project where I am trying and learning as much as I can on the way.",
+      image_url: "./projects/50days.png",
+      live_url: "https://50daysofcode.netlify.app/",
+      techs: [
         "HTML",
         "CSS",
         "SASS",
         "JavaScript",
-        "CSS-Animation",
+        "React",
         "API",
+        "and more...",
       ],
-      "featured": true,
+      featured: true,
     },
     {
-      "title": "Github Tracker",
-      "description": "All my github Repos in one place",
-      "image_url": "./projects/githubtracker.png",
-      "live_url": "https://gitkaranmann.netlify.app/",
-      "techs": [
-        "HTML",
-        "CSS",
-        "JavaScript",
-        "CSS-Animation",
-        "API",
-      ],
-      "featured": true,
+      title: "Star Wars",
+      description:
+        "A Star wars inspired space shooter made in Phaser.Js a web gaming framework",
+      image_url: "./projects/starwars.png",
+      live_url: "https://starwars-phaser.netlify.app/",
+      techs: ["HTML", "CSS", "Phaser.js", "JavaScript"],
+      featured: true,
     },
     {
-      "title": "Pollic",
-      "description": "Systemic consensing is to find a solution that fits all participants",
-      "image_url": "./projects/pollic.png",
-      "live_url": "https://pollic.netlify.app/",
-      "techs": [
+      title: "FOODEX",
+      description: "A food site to focus on HTML, SASS-CSS, JS and Animations",
+      image_url: "./projects/FOODEX.png",
+      live_url: "https://project-foodex.netlify.app/",
+      techs: ["HTML", "CSS", "SASS", "JavaScript", "CSS-Animation", "API"],
+      featured: true,
+    },
+
+    {
+      title: "Github Tracker",
+      description: "All my github Repos in one place",
+      image_url: "./projects/githubtracker.png",
+      live_url: "https://gitkaranmann.netlify.app/",
+      techs: ["HTML", "CSS", "JavaScript", "CSS-Animation", "API"],
+      featured: false,
+    },
+    {
+      title: "Pollic",
+      description:
+        "Systemic consensing is to find a solution that fits all participants",
+      image_url: "./projects/pollic.png",
+      live_url: "https://pollic.netlify.app/",
+      techs: [
         "React",
         "React Router",
         "Styled components",
         "Redux",
         "Agile Methodology",
         "Pair-Programming",
-        "Mob-Programming"
+        "Mob-Programming",
       ],
-      "featured": true,
+      featured: true,
     },
     {
-      "title": "Happy-Thoughts",
-      "description": "A Twitter Clone using REACT.js where I can fetch messages posted on Heroku and also post them and add likes to the posts.",
-      "image_url": "./projects/Happy-thoughts.png",
-      "live_url": "https://cuz-i-am-happy.netlify.app/",
-      "techs": [
+      title: "Happy-Thoughts",
+      description:
+        "A Twitter Clone using REACT.js where I can fetch messages posted on Heroku and also post them and add likes to the posts.",
+      image_url: "./projects/Happy-thoughts.png",
+      live_url: "https://cuz-i-am-happy.netlify.app/",
+      techs: [
         "React",
         "CSS",
         "Express",
         "Node.js",
         "MongoDB",
         "Postman",
-        "Heroku"
+        "Heroku",
       ],
-      "featured": true,
+      featured: true,
     },
     {
-      "title": "HAMBURGER MENU",
-      "description": "A hamburger menu created in pure HTML & CSS, and demoed live on Tjejer Kodar free code workshop on 11th of March 2021",
-      "image_url": "./projects/hamburger-tjejer-kodar.png",
-      "live_url": "https://tjejer-kodar-hamburger-menu.netlify.app",
-      "techs": [
-        "HTML",
-        "CSS",
-      ],
-      "featured": false,
+      title: "HAMBURGER MENU",
+      description:
+        "A hamburger menu created in pure HTML & CSS, and demoed live on Tjejer Kodar free code workshop on 11th of March 2021",
+      image_url: "./projects/hamburger-tjejer-kodar.png",
+      live_url: "https://tjejer-kodar-hamburger-menu.netlify.app",
+      techs: ["HTML", "CSS"],
+      featured: true,
     },
     {
-      "title": "Business Site",
-      "description":"Business site landing page with a responsive hero video image, form with validation and an accordion",
-      "image_url": "./projects/business-site.png",
-      "live_url": "https://business-adventure.netlify.app/",
-      "techs": [
-        "HTML",
-        "CSS",
-        "JavaScript",
-        "Flexbox",
-        "DOM"
-      ],
-      "featured": false,
+      title: "Business Site",
+      description:
+        "Business site landing page with a responsive hero video image, form with validation and an accordion",
+      image_url: "./projects/business-site.png",
+      live_url: "https://business-adventure.netlify.app/",
+      techs: ["HTML", "CSS", "JavaScript", "Flexbox", "DOM"],
+      featured: false,
     },
-    
+
     {
-      "title": "Pizza Bot",
-      "description":"Business site landing page with a responsive hero video image and an accordion",
-      "image_url": "./projects/karsan-pizza.png",
-      "live_url": "https://pizzabotmilano.netlify.app",
-      "techs": [
+      title: "Pizza Bot",
+      description:
+        "Business site landing page with a responsive hero video image and an accordion",
+      image_url: "./projects/karsan-pizza.png",
+      live_url: "https://pizzabotmilano.netlify.app",
+      techs: [
         "HTML",
         "CSS",
         "JavaScript",
         "Flexbox",
         "DOM",
-        "Pair-Programming"
+        "Pair-Programming",
       ],
-      "featured": true,
+      featured: false,
     },
     {
-      "title": "Movie App",
-      "description": "A netflix like movie database app using Open Movie Database API",
-      "image_url": "./projects/movie-app.png",
-      "live_url": "https://imdb-clone-site.netlify.app",
-      "techs": [
+      title: "Movie App",
+      description:
+        "A netflix like movie database app using Open Movie Database API",
+      image_url: "./projects/movie-app.png",
+      live_url: "https://imdb-clone-site.netlify.app",
+      techs: [
         "React",
         "React Router",
         "Styled components",
         "Redux",
         "Agile Methodology",
-        "Mob-Programming"
+        "Mob-Programming",
       ],
-      "featured": true,
+      featured: true,
     },
     {
-      "title": "Music App",
-      "description": "A Spotify like music app landing page using a Local Spotify JSON. Where on hover you can see the like, play and more buttons",
-      "image_url": "./projects/music-app.png",
-      "live_url": "https://spotify-music-clone.netlify.app",
-      "techs": [
-        "React",
-        "CSS",
-        "Agile Methodology",
-        "Mob-Programming"
-      ],
-      "featured": false,
+      title: "Music App",
+      description:
+        "A Spotify like music app landing page using a Local Spotify JSON. Where on hover you can see the like, play and more buttons",
+      image_url: "./projects/music-app.png",
+      live_url: "https://spotify-music-clone.netlify.app",
+      techs: ["React", "CSS", "Agile Methodology", "Mob-Programming"],
+      featured: true,
     },
     {
-      "title": "Todo App",
-      "description": "A classic Project of a todo app where you can add, edit, delete and set the status complete for a task",
-      "image_url": "./projects/todoapp.png",
-      "live_url": "https://todo-in-moment.netlify.app",
-      "techs": [
+      title: "Todo App",
+      description:
+        "A classic Project of a todo app where you can add, edit, delete and set the status complete for a task",
+      image_url: "./projects/todoapp.png",
+      live_url: "https://todo-in-moment.netlify.app",
+      techs: [
         "React",
         "Styled Components",
         "Redux Toolkit",
         "Material UI",
         "Agile Methodology",
-        "Mob-Programming"
+        "Mob-Programming",
       ],
-      "featured": false,
+      featured: false,
     },
     {
-      "title": "Authentication",
-      "description": "An authentication page where you can signup and create and ID. then you can login and display a secret message ",
-      "image_url": "./projects/authentication.png",
-      "live_url": "https://wk20-api-auth.netlify.app",
-      "techs": [
-        "React",
-        "Javascript",
-        "React Router",
-        "Redux Toolkit",
-        "MongoDB",
-        "Node.js",
-        "Mongoose",
-        "Agile Methodology",
-        "Mob-Programming"
-      ],
-      "featured": false,
+      title: "C-L-U-E-D-O",
+      description:
+        "A small representation of the classic Cluedo game. It generates random scenarios and gives you the result",
+      image_url: "./projects/cluedo.png",
+      live_url: "https://gettheclue.netlify.app",
+      techs: ["HTML", "CSS", "Flexbox", "Javascript"],
+      featured: false,
     },
     {
-      "title": "Game Zone",
-      "description": "The first site I made as part of the Technigo Bootcamp. My first attempt in Flexbox and Responsive Design",
-      "image_url": "./projects/gamezone.png",
-      "live_url": "https://gamezonee.netlify.app",
-      "techs": [
-        "HTML",
-        "CSS",
-        "Flexbox",
-        "Responsiveness",
-      ],
-      "featured": false,
+      title: "Aussie Burger",
+      description:
+        "A burger review site using the Zomato API which filters the restaurants as per the star rating and price",
+      image_url: "./projects/burgersite.png",
+      live_url: "https://zomato-review-clone.netlify.app",
+      techs: ["HTML", "CSS", "Flexbox", "Javascript", "DOM", "API:s"],
+      featured: false,
     },
     {
-      "title": "Weather App",
-      "description": "Weather app using the open weather API. Also showing the forcase for the next five days.",
-      "image_url": "./projects/weather.png",
-      "live_url": "https://vishwa-mausam.netlify.app",
-      "techs": [
-        "HTML",
-        "CSS",
-        "Flexbox",
-        "Responsiveness",
-        "Javascript",
-        "API:s"
-      ],
-      "featured": false,
+      title: "EDAMAM",
+      description: "Recipe search engine using the Edamam API",
+      image_url: "./projects/Edamam.png",
+      live_url: "https://edamam.netlify.app",
+      techs: ["HTML", "CSS", "DOM", "Javascript", "API:s"],
+      featured: true,
     },
-    {
-      "title": "C-L-U-E-D-O",
-      "description": "A small representation of the classic Cluedo game. It generates random scenarios and gives you the result",
-      "image_url": "./projects/cluedo.png",
-      "live_url": "https://gettheclue.netlify.app",
-      "techs": [
-        "HTML",
-        "CSS",
-        "Flexbox",
-        "Javascript",
-      ],
-      "featured": false,
-    },
-    {
-      "title": "Aussie Burger",
-      "description": "A burger review site using the Zomato API which filters the restaurants as per the star rating and price",
-      "image_url": "./projects/burgersite.png",
-      "live_url": "https://zomato-review-clone.netlify.app",
-      "techs": [
-        "HTML",
-        "CSS",
-        "Flexbox",
-        "Javascript",
-        "DOM",
-        "API:s"
-      ],
-      "featured": false,
-    },
-    {
-      "title": "Chore-Door",
-      "description": "A game designed to generate random hidden scenarios behind the doors. Get the bot and you loose. I did this as a part of the Codeacademy project",
-      "image_url": "./projects/chor-door.png",
-      "live_url": "https://chore-door-karan.netlify.app",
-      "techs": [
-        "HTML",
-        "CSS",
-        "Flexbox",
-        "Javascript",
-        "DOM",
-      ],
-      "featured": false,
-    },
-    {
-      "title": "Hörlurar",
-      "description": "React based quiz that takes the answers to your questions and displays the summary of it",
-      "image_url": "./projects/horlurar.png",
-      "live_url": "https://hearmeout.netlify.app",
-      "techs": [
-        "React",
-        "CSS",
-        "React-Hooks",
-        "Javascript",
-        "DOM",
-      ],
-      "featured": false,
-    },
-    {
-      "title": "Labyrinth",
-      "description": "Text based representation of the Labrynth Game",
-      "image_url": "./projects/laberynth.png",
-      "live_url": "https://labyrinth-react-redux.netlify.app",
-      "techs": [
-        "React",
-        "React-Hooks",
-        "Material UI",
-        "Javascript",
-        "Redux-Toolkit",
-      ],
-      "featured": true,
-    },
-    {
-      "title": "EDAMAM",
-      "description": "Recipe search engine using the Edamam API",
-      "image_url": "./projects/Edamam.png",
-      "live_url": "https://edamam.netlify.app",
-      "techs": [
-        "HTML",
-        "CSS",
-        "DOM",
-        "Javascript",
-        "API:s",
-      ],
-      "featured": false,
-    },
-    
-  ]
-}
+  ],
+};
